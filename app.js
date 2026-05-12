085b9ca5acf42591c732f0915323d279c6932546
Pedram Oskouian <38936039+iampedraam@users.noreply.github.com>
2026-05-12 15:40:14 +0330

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const methodOverride = require('method-override');
 const app = express();
 
 app.set('view engine', 'ejs');
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ limit: '50kb', extended: true })); 
 app.use(express.static('public'));
 app.use(methodOverride('_method'));
 
@@ -21,13 +21,13 @@ app.get('/', (req, res) => {
 });
 
 app.post('/posts', (req, res) => {
-    const { title, content, publisher } = req.body;
-    const newPost = {
-        id: Date.now().toString(),
-        title,
-        publisher,
-        content
-    };
+    let { title, content, publisher } = req.body;
+
+    title = title.substring(0, 50);
+    publisher = publisher.substring(0, 30);
+    content = content.substring(0, 2500);
+
+    const newPost = { id: Date.now().toString(), title, publisher, content };
     posts.push(newPost);
     res.redirect('/');
 });
@@ -41,15 +41,29 @@ app.get('/edit/:id', (req, res) => {
 app.put('/edit/:id', (req, res) => {
     const { title, content, publisher } = req.body;
     const index = posts.findIndex(p => p.id === req.params.id);
+
     if (index !== -1) {
-        posts[index] = { id: req.params.id, title, content, publisher };
+        posts[index] = { 
+            id: req.params.id, 
+            title: title.substring(0, 50), 
+            publisher: publisher.substring(0, 30), 
+            content: content.substring(0, 400) 
+        };
     }
+    
     res.redirect('/');
 });
 
 app.delete('/delete/:id', (req, res) => {
     posts = posts.filter(p => p.id !== req.params.id);
     res.redirect('/');
+});
+
+app.use((err, req, res, next) => {
+    if (err.type === 'entity.too.large') {
+        return res.status(413).send('Post is too large! Please keep it under 1MB.');
+    }
+    res.status(500).send('Something went wrong on the server.');
 });
 
 app.listen(3000, () => console.log(`Server is running at http://localhost:3000`));
